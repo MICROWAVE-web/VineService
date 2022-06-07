1c22a495f30fab2f4d1ec4a0d6736a64f5bf945a
BlackCoc <74858759+BlackCoc@users.noreply.github.com>
2022-06-07 22:48:57 +0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,117 @@
+import os
+
+import telebot
+from telebot import types
+
+from data_management import find_client, save_ratings, load_data
+
+token = '5395555491:AAEOZ4b7htMaX2keSFbsuQvSd7y2Se9LH6I'
+bot = telebot.TeleBot(token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️ "
+                                      "Чтобы узнать список всех команды пишите /help")
+
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    bot.send_message(message.chat.id, "/number 88005553535 - Найти клиента по номеру телефона\n"
+                                      "/card 001 - Найти клиента по номеру карты\n"
+                                      "/download - Сгрузить рейтинг вин")
+
+
+def extract_arg(arg, message):
+    try:
+        return str(arg.split()[1:][0])
+    except IndexError:
+        bot.send_message(message.chat.id, 'Неверное значение',
+                         parse_mode='Markdown')
+
+
+def send_vines(message, all_data):
+    bot.send_message(message.chat.id,
+                     f"Имя: *{all_data[0]['name']}*\nНомер карты: *{all_data[0]['loyal_card_number']}*\nНомер телефона: *{all_data[0]['phone_number']}*",
+                     parse_mode='Markdown')
+
+    for element in all_data[:15]:
+        if not element['articulate']:
+            element['articulate'] = 'Отсутствует'
+        markup_inline = types.InlineKeyboardMarkup()
+        item_1 = types.InlineKeyboardButton(text='1', callback_data=f"{element['articulate']} 1")
+        item_2 = types.InlineKeyboardButton(text='2', callback_data=f"{element['articulate']} 2")
+        item_3 = types.InlineKeyboardButton(text='3', callback_data=f"{element['articulate']} 3")
+        item_4 = types.InlineKeyboardButton(text='4', callback_data=f"{element['articulate']} 4")
+        item_5 = types.InlineKeyboardButton(text='5', callback_data=f"{element['articulate']} 5")
+        markup_inline.add(item_1, item_2, item_3, item_4, item_5)
+        bot.send_message(message.chat.id,
+                         f"Дата покупки: *{element['date']}*\nАртикул: *{element['articulate']}*\nНазвание вина: *{element['nomn']}*\nЦена: *{element['price']}*\nЦена со скидкий: *{element['price_with_discount']}*\n\nОцените вино:",
+                         parse_mode='Markdown', reply_markup=markup_inline)
+
+
+@bot.message_handler(commands=['number'])
+def findbynumber(message):
+    number = extract_arg(message.text, message)
+    all_data = find_client(phone_number=number)
+    if len(all_data) == 0:
+        bot.send_message(message.chat.id, 'Ничего не найдено',
+                         parse_mode='Markdown')
+        return
+    send_vines(message, all_data)
+
+
+@bot.message_handler(commands=['card'])
+def findbycard(message):
+    card = extract_arg(message.text, message)
+    all_data = find_client(card=card)
+    if len(all_data) == 0:
+        bot.send_message(message.chat.id, 'Ничего не найдено',
+                         parse_mode='Markdown')
+        return
+    send_vines(message, all_data)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    art, rate = call.data.split()
+    data = load_data()
+    title = ''
+    for e in data:
+        if e['articulate'] == art:
+            title = e['nomn']
+    if art == 'Отсутствует':
+        bot.answer_callback_query(callback_query_id=call.id, text=f'Вы не можете оставить рейтинг к этому товару!')
+        return
+    save_ratings(articulate=art, title=title, rate=rate)
+    bot.answer_callback_query(callback_query_id=call.id, text=f'Вы оставили рейтинг {rate} к товару {art}!')
+
+
+@bot.message_handler(content_types=['document'])
+def upload_base(message):
+    if not str(message.document.file_name).endswith('.xls') or not str(message.document.file_name).endswith('.xlsx'):
+        bot.reply_to(message, "Неверный формат файла")
+        return
+    os.remove('all_data/data.xls')
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    src_old = 'all_data/' + message.document.file_name
+    src_new = 'all_data/' + 'data.xls'
+    with open(src_old, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.rename(src_old, src_new)
+    bot.reply_to(message, "Новая база успешно установлена")
+
+
+@bot.message_handler(commands=['download'])
+def start_message(message):
+    rating_xlsx = open('all_data/ratings.xlsx', 'rb')
+    bot.send_document(message.chat.id, rating_xlsx)
+
+
+def main():
+    bot.infinity_polling()
+
+
+if __name__ == '__main__':
+    main()
---
+++ data_management.py
@@ -0,0 +1,90 @@
+import csv
+
+import pandas as pd
+import xlrd
+from openpyxl import Workbook
+
+
+# pyexcel-xlsx
+
+def load_data(path=r'all_data/data.xls'):
+    LoadedData = []
+    workbook = xlrd.open_workbook(path)
+    worksheet = workbook.sheet_by_index(0)
+    data = worksheet.cell(5, 5).value
+    for x in range(1000):
+        try:
+            date_check = worksheet.cell(x, 8).value
+            if not date_check:
+                continue
+        except IndexError:
+            break
+        LoadedData.append({
+            'loyal_card_number': worksheet.cell(x, 3).value,
+            'phone_number': worksheet.cell(x, 6).value,
+            'name': worksheet.cell(x, 5).value,
+            'date': worksheet.cell(x, 8).value,
+            'articulate': worksheet.cell(x, 9).value,
+            'nomn': worksheet.cell(x, 10).value,
+            'price': worksheet.cell(x, 13).value,
+            'price_with_discount': worksheet.cell(x, 15).value,
+        })
+        '''for y in range(20):
+            data = worksheet.cell(x, y).value
+            if data:
+                print(f'{x}, {y} - {data}', end='\n\n')'''
+    return LoadedData
+
+
+def mean(numbers):
+    return round(sum(numbers)) / max(len(numbers), 1)
+
+
+def save_ratings(articulate, title, rate, path=r'all_data/ratings.csv', path_xlsx=r'all_data/ratings.xlsx'):
+    import csv
+    import os
+    if not os.path.exists(path):
+        with open(path, mode="w+", encoding='utf-8') as w_file:
+            file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
+            file_writer.writerow(["Артикул", "Название", "Оценки", 'Средний балл'])
+            # file_writer.writerow([articulate, "0", 0.0])
+        #print('Файл создан')
+    else:
+        # print('Файл существует')
+        pass
+
+    df = pd.read_csv(path, sep=';')
+    exist = False
+    last_index = 0
+    for x in range(1000):
+        try:
+            csv_art = df.iloc[x, 0]
+            csv_marks = df.iloc[x, 2]
+            csv_avg = df.iloc[x, 3]
+        except IndexError:
+            last_index = x
+            break
+        if csv_art == articulate:
+            exist = True
+            csv_marks += f'{rate}/'
+            csv_avg = round(mean(list(map(int, csv_marks.split('/')[:-1]))), 1)
+            df.iloc[x, 0] = csv_art
+            df.iloc[x, 1] = title
+            df.iloc[x, 2] = csv_marks
+            df.iloc[x, 3] = csv_avg
+    if not exist:
+        df.loc[last_index] = [articulate, title, f"{rate}/", float(rate)]
+    df.to_csv(path, sep=';', index=False)
+    df.to_excel(path_xlsx, index=None, header=True)
+
+
+def find_client(phone_number=None, card=None):
+    data = load_data()
+    output = []
+    for transaction in data:
+        clear_phone = str(''.join([str(s) for s in transaction['phone_number'] if s.isdigit()]))
+        if clear_phone == phone_number or transaction['loyal_card_number'].split()[0] == card:
+            output.append(transaction)
+    return output
+
+# save_ratings(articulate='M4047', rate='2')
